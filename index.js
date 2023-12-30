92aa7fe613be2129bccf4b3bc9277ed43e7aad15
Martha Harrold <martharee392@gmail.com>
2023-12-30 16:55:20 -0500

    added javascript for player scores

---
+++ index.js
@@ -0,0 +1,7 @@
+const scoreDisplay = document.getElementById('score-display');
+let score = 100
+scoreDisplay.textContent = score
+
+const scoreDisplay2 = document.getElementById('score-display2');
+let score2 = 0
+scoreDisplay2.textContent = score2
