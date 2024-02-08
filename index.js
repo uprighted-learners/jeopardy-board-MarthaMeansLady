0ec5faf6114546cee1ee5d7dd3d9af83559ecdfb
Martha Harrold <martharee392@gmail.com>
2024-02-08 00:47:53 -0500

    added popup for player to select first q

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const scoreDisplay = document.getElementById('score-display');
-let score = 100
+let score = 0
 scoreDisplay.textContent = score
 
 const scoreDisplay2 = document.getElementById('score-display2');
 let score2 = 0
 scoreDisplay2.textContent = score2
+
+// function ask(questionText) {
+//     return new Promise((resolve) => {
+//         setTimeout(() => resolve(prompt(questionText)), 1000);
+//     });
+// }
+
+function showPopup() {
+    document.getElementById("popup").style.display = "flex";
+};
+
+function closePopup() {
+    console.log("closePopup function called");
+    document.getElementById("popup").style.display = "none";
+};
+
+
+document.addEventListener("DOMContentLoaded", () => {
+      showPopup();
+});
+
+document.querySelector(".closeButton").addEventListener('click', function(event) {
+    closePopup();
+    event.stopPropagation();
+});
+
