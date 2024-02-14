a24872fd20dd3e85d46d2de9e698657e61046646
Martha Harrold <martharee392@gmail.com>
2024-02-14 17:54:38 -0500

    got pass button to work, and player notification

--- index.js
+++ index.js
@@ -1,37 +1,62 @@
 import placeholderQuestions from "./my-placeholder-questions.js";
 
+//sets the game conditions
 let gameCondition = {
     currentPlayer: 1,
-    playerScores: [0, 1],
-    selectCategory: null,
-    selectQuestion: null,
+    playerScores: [0, 0],
     questionActive: false,
     currentQuestionValue: 0,
-}
-
-const buttonSubmit = document.getElementById('buttonSubmit');
-const buttonGuess = document.getElementById('buttonGuess');
-const buttonPass = document.getElementById('buttonPass');
-// const answer = userInput.value;
+    questionUpForSteal: false,
+    currentAnswer: '',
+};
 
 //displays the first modal when the page is loaded
-
+document.addEventListener("DOMContentLoaded", () => {
     modalStart();
-    // event.stopPropagation();
+});
 
-function closePopupOne() {
-    document.getElementById("modalOne").style.display = "none";
-};
+
 function modalStart () {
     document.getElementById("modalOne").style.display = "flex";
     document.querySelector(".closeButtonOne").addEventListener('click', function(event) {
-    event.stopPropagation();
-    closePopupOne()
-})
+        event.stopPropagation();
+        closePopupOne()
+    })
 };
 
+//closes the modal that pops up when the page is loaded
+function closePopupOne() {
+    document.getElementById("modalOne").style.display = "none";
+};
 
+function showModalQ(question, answer) {
+    document.getElementById("modalQuestion").textContent = question;
+    document.getElementById("modalAnswer").textContent = answer; 
+    document.getElementById("modalTwo").style.display = "flex";
+}
 
+//closes the modal that pops up for all questions
+function closePopupTwo() {
+    document.getElementById("modalTwo").style.display = "none";
+    gameCondition.questionActive = false;
+};
+
+//keeps tally of player scores
+function updateScoreDisplay() {
+    document.getElementById("playerOneScore").textContent = gameCondition.playerScores[0];
+    document.getElementById("playerTwoScore").textContent = gameCondition.playerScores[1];
+}
+
+// close the modal popup
+document.querySelector(".closeButtonTwo").addEventListener('click', function(event) {
+    event.stopPropagation();
+    closePopupTwo()
+});
+
+document.getElementById("buttonPass").addEventListener('click', function(event) {
+    event.preventDefault();
+    closePopupTwo();
+});
 
 
 
@@ -45,78 +70,46 @@ document.querySelectorAll("#jeopardyCategory .cgory").forEach(categoryElement =>
     });
 });
 
-//pulls the questions from the placeholder file
-document.querySelectorAll("#jeopardyCategory .q").forEach(categoryElement => {
-    categoryElement.addEventListener("click", function() {
-        const questions = placeholderQuestions.map(question => question.question).filter((value, index, self) => self.indexOf(value) === index);
-        const randomIndexQ = Math.floor(Math.random() * questions.length);
-        const randomQuestion = questions[randomIndexQ];
-        showModalQ(randomQuestion);
-        // this.textContent = randomQuestion;
-        if (buttonPass) {
-            gameCondition.currentPlayer = gameCondition.currentPlayer === 1 ? 2 : 1; }
-        // if (randomQuestion === parseInt())
+// Picks a random question and answer pair
+document.querySelectorAll("#jeopardyCategory .q").forEach(questionElement => {
+    questionElement.addEventListener("click", function() {
+        if (!gameCondition.questionActive) {
+            const randomIndex = Math.floor(Math.random() * placeholderQuestions.length);
+            const randomQuestion = placeholderQuestions[randomIndex].question;
+            const randomAnswer = placeholderQuestions[randomIndex].answer;
+            gameCondition.currentQuestionValue = parseInt(this.getAttribute('data-value'));
+            gameCondition.currentAnswer = randomAnswer;
+            console.log('data-value', this.getAttribute('data-value'));
+            showModalQ(randomQuestion, randomAnswer);
+            gameCondition.questionActive = true;
+        }         
     });
 });
 
-// modal that displays for questions
-// document.addEventListener( () => {
-//     showModalQ();
-// });
-function closePopupTwo() {
-    document.getElementById("modalTwo").style.display = "none";
-};
-
-// Puts the question text in the modal
-function showModalQ(question) {
-    document.getElementById("modalQuestion").textContent = question + ' (CLICK the "X" to answer, or pass.)';
-    document.getElementById("modalTwo").style.display = "flex";
-    document.querySelector(".closeButtonTwo").addEventListener('click', function(event) {
-    event.stopPropagation();
-    closePopupTwo()
-    })
-};
-
-//events for scorekeeping
-// document.addEventListener("click", function(event) {
-//     if (event.target.id === "buttonSubmit") {
-//         currentQuestionValue = parseInt(this.getAttribute('data-value'));
-//         event.preventDefault(); 
-//         gameCondition.currentPlayer = gameCondition.currentPlayer === 1? 2 : 1;
-//         showModalPlayerTurn();
-//     }});
-
-        //adds a score to each question
-document.querySelectorAll('.q').forEach(question => {
-    question.addEventListener('click', function() {
-        currentQuestionValue = parseInt(this.getAttribute('data-value'));
-        document.getElementById('modalQuestion').textContent = this.textContent + ' (CLICK the "X" to answer, or pass.)';
-        document.getElementById('modalTwo').style.display = 'flex'; 
-    });
-});
-        document.querySelector(".closeButtonTwo").addEventListener('click', function(event) {
-        event.stopPropagation();
-        closePopupTwo()
-        });
-
-
-//if player passes the question
-document.addEventListener("click", function(event) {
-    if (event.target.id === "buttonPass") {
-        event.preventDefault(); 
-        gameCondition.currentPlayer = gameCondition.currentPlayer === 1? 2 : 1;
-        showModalPlayerTurn();
+//updating player scores based on correct or incorrect answer, or if a player steals
+document.getElementById("buttonSubmit").addEventListener('click', function(event) {
+    event.preventDefault();
+    let userAnswer = document.getElementById("submitAnswer").value.trim();
+    if (userAnswer === gameCondition.currentAnswer) { 
+        gameCondition.playerScores[gameCondition.currentPlayer - 1] += gameCondition.currentQuestionValue;
+        gameCondition.questionActive = false;
+        gameCondition.questionUpForSteal = false; 
+        } else {
+        if (!gameCondition.questionUpForSteal) {
+            gameCondition.questionUpForSteal = true;
+            gameCondition.currentPlayer = gameCondition.currentPlayer === 1? 2 : 1;           
+        } else {
+            gameCondition.playerScores[gameCondition.currentPlayer - 1] -= gameCondition.currentQuestionValue;
+            gameCondition.questionActive = false;
+            gameCondition.questionUpForSteal = false; 
+        }
     }
+    document.getElementById("submitAnswer").value = ''; 
+    updateScoreDisplay();
+    closePopupTwo(); 
+    
 });
 
-//if player takes a guess
-document.addEventListener("click", function(event) {
-    if (event.target.id === "buttonSubmit") {
-        event.preventDefault(); 
-        gameCondition.currentPlayer = gameCondition.currentPlayer === 1? 2 : 1;
-        showModalPlayerTurn();
-    }
-});
 
 //after the first round is complete
 document.addEventListener("click", function(event) {
@@ -126,56 +119,3 @@ document.addEventListener("click", function(event) {
         showModalPlayerTurn();
     }
 });
-
-// document.querySelector(".buttonSubmit")(categoryElement => {
-//     categoryElement.addEventListener("click", function() {
-//         const checkAnswer = placeholderQuestions.map(question => question.question).filter((value, index, self) => self.indexOf(value) === index);
-//         const randomIndexQ = Math.floor(Math.random() * questions.length);
-//         const randomQuestion = questions[randomIndexQ];
-//         this.textContent = randomQuestion;
-//         if (buttonPass) {
-//             gameCondition.currentPlayer = gameCondition.currentPlayer === 1 ? 2 : 1; }
-//     });
-// });
-
-// const notification = document.getElementById('modal').createChild;
-// const notification = document.getElementById('modal');
-// notification.style.display = "block";
-
-// buttonPass.addEventListener('click', function(event) {
-//     event.preventDefault(); 
-//     gameCondition.currentPlayer = gameCondition.currentPlayer === 1 ? 2 : 1;
-// });
-
-// buttonSubmit.addEventListener('click', function(event) {
-//     event.preventDefault(); 
-//     if (buttonSubmit === question)
-//     gameCondition.currentPlayer = gameCondition.currentPlayer === 1 ? 2 : 1;
-// });
-
-
-// let response = await fetch(`https://jsonplaceholder.typicode.com/posts/1`);
-
-// let text = await response.text();
-
-// let json = await response.json();
-
-// async function fetchCategory() {
-//     const apiURL = 'http://jservice.io/api/category?id=1';
-//     try{
-//         const response = await fetch(apiURL);
-//         if (!response.ok) {
-//             throw new Error(response.statusText);
-//         }
-//         const data = await response.json();
-//         updateJeopardyCategory(data);
-//     } catch (error) {
-//         console.log("Failed to fetch category", error);
-//     }
-// }
-
-
-
-
-
-
